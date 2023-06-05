a2f934ca3f118b6544f328b893ee9009cb3e67fd
Biswajit <biswajitghosh150803@gmail.com>
2023-06-05 23:13:48 +0530

    .....

--- src/pages/Inc/index.jsx
+++ src/pages/Inc/index.jsx
@@ -1,160 +1,174 @@
 import React from "react";
 import styles from "./styles.module.css";
-import axios from 'axios';
-import Navy from '../Navy/index';
+import axios from "axios";
+import Navy from "../Navy/index";
 
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
-import { SimpleGrid, Box, Stack , Button, Input, FormLabel, NumberInputField, NumberInput} from '@chakra-ui/react';
-import {AiFillEdit , AiFillDelete } from "react-icons/ai";
-import { DeleteIcon } from '@chakra-ui/icons'
+import {
+  SimpleGrid,
+  Box,
+  Stack,
+  Button,
+  Input,
+  FormLabel,
+  NumberInputField,
+  NumberInput,
+} from "@chakra-ui/react";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { DeleteIcon } from "@chakra-ui/icons";
 import { useForceUpdate } from "framer-motion";
-function Inc(userDetails){
-    const user = userDetails;
-    //const [amt,setAmt]=React.useState(null);
-    const [noteIns,setNoteIns]=React.useState(null);
-    
+function Inc(userDetails) {
+  const user = userDetails;
+  //const [amt,setAmt]=React.useState(null);
+  const [noteIns, setNoteIns] = React.useState(null);
 
-    React.useEffect(()=>{
-            const options = {
-                method: 'GET',
-                url: 'http://localhost:8080/incometable',
-                params: {Username: user.user.email},
-            };
+  React.useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "http://localhost:8080/incometable",
+      params: { Username: user.user.email },
+    };
 
-            axios.request(options).then((response) => {
-                //console.log(response.data)
-                response.data.reverse();
-               setNoteIns(response.data)
+    axios
+      .request(options)
+      .then((response) => {
+        //console.log(response.data)
+        response.data.reverse();
+        setNoteIns(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [user.user.email]);
 
-            }).catch((error) => {
-                console.error(error)
-            });
-    },[user.user.email]);
-
-    const [edit,setEdit]=React.useState(null);
-    function deleteFunc(event){
-        setChange(change+1);
-        console.log("id= "+event);
-        if ((event.target.id!==undefined)||(event.target.id!==null))
-        {
-            const d={
-                _id:event.target.id
-            }
-            axios.post("http://localhost:8080/api/incdel", d);
-        }
-        window.location.reload();
+  const [edit, setEdit] = React.useState(null);
+  function deleteFunc(event) {
+    setChange(change + 1);
+    console.log("id= " + event);
+    if (event.target.id !== undefined || event.target.id !== null) {
+      const d = {
+        _id: event.target.id,
+      };
+      axios.post("http://localhost:8080/api/incdel", d);
     }
+    window.location.reload();
+  }
 
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
 
-    const [noteInc, setNoteInc] = React.useState({
-		Type: "",
-		Desc: "",
-	    Amt: 0,
-        Date: "",
-        id: ""
-	  });
-	function handleChangeInc(event) {
-		const { name, value } = event.target;
-	
-		setNoteInc((prevNote) => {
-		  return {
-			...prevNote,
-			[name]: value
-		  };
-		});
-	  };
-	  const submitNoteInc =async (event)=> {
-        const e={
-            Type: noteInc.Type,
-            Desc:  noteInc.Desc,
-            Amt: noteInc.Amt,
-            Date: noteInc.Date,
-            id: noteInc.id
-        }
-        console.log(noteInc);
-		axios.post("http://localhost:8080/upincome", e);
-		window.location.reload();
-	  };
-    function editFunc(event){
-        setChange(change+1);
-        console.log(event.target.id);
-        setEdit(event.target.id);
-        setNoteInc((prevNote) => {
-            return {
-              ...prevNote,
-              id: event.target.id
-            };
-          });
-        onOpenModal();
-    }
-    const [input, setInput] = React.useState(true);
-    const [change,setChange]=React.useState(0);
-  
-    const [amt1,set1]=React.useState(null);
-    React.useEffect(()=>{
-       const options = {
-            method: 'GET',
-            url: 'http://localhost:8080/api/income',
-            params: {Username: user.user.email},
-        };
+  const [noteInc, setNoteInc] = React.useState({
+    Type: "",
+    Desc: "",
+    Amt: 0,
+    Date: "",
+    id: "",
+  });
+  function handleChangeInc(event) {
+    const { name, value } = event.target;
 
-        axios.request(options).then((response) => {
-            console.log(response.data.orders[0].totalAmount.$numberDecimal);
-            set1(response.data.orders[0].totalAmount.$numberDecimal);
-        }).catch((error) => {
-            console.error(error)
+    setNoteInc((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+  const submitNoteInc = async (event) => {
+    const e = {
+      Type: noteInc.Type,
+      Desc: noteInc.Desc,
+      Amt: noteInc.Amt,
+      Date: noteInc.Date,
+      id: noteInc.id,
+    };
+    console.log(noteInc);
+    axios.post("http://localhost:8080/upincome", e);
+    window.location.reload();
+  };
+  function editFunc(event) {
+    setChange(change + 1);
+    console.log(event.target.id);
+    setEdit(event.target.id);
+    setNoteInc((prevNote) => {
+      return {
+        ...prevNote,
+        id: event.target.id,
+      };
+    });
+    onOpenModal();
+  }
+  const [input, setInput] = React.useState(true);
+  const [change, setChange] = React.useState(0);
+
+  const [amt1, set1] = React.useState(null);
+  React.useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "http://localhost:8080/api/income",
+      params: { Username: user.user.email },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data.orders[0].totalAmount.$numberDecimal);
+        set1(response.data.orders[0].totalAmount.$numberDecimal);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [user.user.email, change]);
+
+  ///Search//////////////////////////////////////////////////
+  const [search1, setSearch1] = React.useState(null);
+  function handleSearch(event) {
+    setSearch1(event.target.value);
+  }
+  function search() {
+    if (search1 === "") {
+      window.location.reload();
+    } else {
+      const options = {
+        method: "GET",
+        url: "http://localhost:8080/incometable2",
+        params: { Username: user.user.email, Type: search1 },
+      };
+
+      axios
+        .request(options)
+        .then((response) => {
+          //console.log(response.data)
+          response.data.reverse();
+          setNoteIns(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
         });
-    },[user.user.email,change]);
+      const options2 = {
+        method: "GET",
+        url: "http://localhost:8080/api/income2",
+        params: { Username: user.user.email, Type: search1 },
+      };
 
-    ///Search//////////////////////////////////////////////////
-    const [search1,setSearch1]=React.useState(null);
-    function handleSearch(event){
-        setSearch1(event.target.value);
-    }
-    function search(){
-        if(search1===""){
-            window.location.reload();
-        }
-        else{
-        const options = {
-            method: 'GET',
-            url: 'http://localhost:8080/incometable2',
-            params: {Username: user.user.email,
-                    Type: search1,
-            },
-        };
-
-        axios.request(options).then((response) => {
-            //console.log(response.data)
-            response.data.reverse();
-           setNoteIns(response.data)
-
-        }).catch((error) => {
-            console.error(error)
-        });
-        const options2 = {
-            method: 'GET',
-            url: 'http://localhost:8080/api/income2',
-            params: {Username: user.user.email,Type: search1,},
-        };
-
-        axios.request(options2).then((response) => {
-            console.log(response.data.orders[0].totalAmount.$numberDecimal);
-            set1(response.data.orders[0].totalAmount.$numberDecimal);
-        }).catch((error) => {
-            console.error(error)
+      axios
+        .request(options2)
+        .then((response) => {
+          console.log(response.data.orders[0].totalAmount.$numberDecimal);
+          set1(response.data.orders[0].totalAmount.$numberDecimal);
+        })
+        .catch((error) => {
+          console.error(error);
         });
     }
-    }
+  }
 
-
-    return(<div>
-        <Modal
+  return (
+    <div>
+      <Modal
         className="mode"
         open={open}
         onClose={onCloseModal}
@@ -193,11 +207,11 @@ function Inc(userDetails){
               onChange={handleChangeInc}
             />
             <FormLabel className="home-pad" name="Email">
-                Amount
+              Amount
             </FormLabel>
             <NumberInput className={styles.fie} width="30%" required>
-					<NumberInputField name="Amt" onChange={handleChangeInc} />
-				</NumberInput>
+              <NumberInputField name="Amt" onChange={handleChangeInc} />
+            </NumberInput>
             <div className="home-pad"></div>
             <Button colorScheme="teal" type="submit">
               Submit
@@ -205,46 +219,131 @@ function Inc(userDetails){
           </form>
         </div>
       </Modal>
-        <Navy user={user} />
-        <div className={styles.gap}></div>
-        <div className={styles.topinc}>
+      <Navy user={user} />
+      <div className={styles.gap}></div>
+      <div className={styles.topinc}>
         <h1 className={styles.topic}>Income Table</h1>
 
-        <div className={styles.searcc}><Input
-              placeholder="Search"
-              _placeholder={{ opacity: 1, color: "gray.600" }}
-              name="Type"
-              onChange={handleSearch}
-            /><Button colorScheme="teal" onClick={search}>Search</Button> </div></div>
-
-
-        <div className={styles.gap}></div>
-        <div className={styles.table}>
-            <Stack spacing={2}>
-                <SimpleGrid columns={5} spacing={1}>
-                    <Box className={styles.head} bg='teal' height='80px'> Type </Box>
-                    <Box className={styles.head} bg='teal' height='80px'> Description </Box>
-                    <Box className={styles.head} bg='teal' height='80px'> Amount </Box>
-                    <Box className={styles.head} bg='teal' height='80px'> Date </Box>
-                    <Box className={styles.head} bg='teal' height='80px'> Edit/Delete </Box>
-                </SimpleGrid>
-                {(noteIns!=null)?noteIns.map((noteItem)=>{
-                        return(<SimpleGrid className={styles.in} columns={5} spacing={1} key={noteItem._id}>
-                                    <Box key={noteItem._id} className={styles.in} bg='gray.50' height='80px'>{noteItem.Type}</Box>
-                                    <Box key={noteItem._id.slice(0,6)} className={styles.in} bg='gray.50' height='80px'>{noteItem.Desc}</Box>
-                                    <Box key={noteItem._id.slice(0,5)} className={styles.in} bg='gray.50' height='80px'>{noteItem.Amt['$numberDecimal'].toLocaleString()}</Box>
-                                    <Box key={noteItem._id.slice(0,7)} className={styles.in} bg='gray.50' height='80px'>{noteItem.updatedAt.slice(0,10)}</Box>
-                                    <Box key={noteItem._id.slice(0,8)} className={styles.in} bg='gray.50' height='80px'><Button id={noteItem._id} colorScheme='teal' onClick={editFunc}><AiFillEdit /></Button> &nbsp;&nbsp; <Button colorScheme='teal'  id={noteItem._id} onClick={deleteFunc}><AiFillDelete   /></Button></Box>
-                                </SimpleGrid>);
-                }):<></>}
-                <SimpleGrid columns={2} spacing={1}>
-                    <Box className={styles.head} bg='teal' height='80px'>Total Income :</Box>
-                    <Box className={styles.head} bg='teal' height='80px'>INR {(amt1===null)?0:amt1}</Box>
-                </SimpleGrid>
-            </Stack>
+        <div className={styles.searcc}>
+          <Input
+            placeholder="Search"
+            _placeholder={{ opacity: 1, color: "gray.600" }}
+            name="Type"
+            onChange={handleSearch}
+          />
+          <Button colorScheme="teal" onClick={search}>
+            Search
+          </Button>{" "}
         </div>
-    </div>
-    );}
+      </div>
 
+      <div className={styles.gap}></div>
+      <div className={styles.table}>
+        <Stack spacing={2}>
+          <SimpleGrid columns={5} spacing={1}>
+            <Box className={styles.head} bg="teal" height="80px">
+              {" "}
+              Type{" "}
+            </Box>
+            <Box className={styles.head} bg="teal" height="80px">
+              {" "}
+              Description{" "}
+            </Box>
+            <Box className={styles.head} bg="teal" height="80px">
+              {" "}
+              Amount{" "}
+            </Box>
+            <Box className={styles.head} bg="teal" height="80px">
+              {" "}
+              Date{" "}
+            </Box>
+            <Box className={styles.head} bg="teal" height="80px">
+              {" "}
+              Edit/Delete{" "}
+            </Box>
+          </SimpleGrid>
+          {noteIns != null ? (
+            noteIns.map((noteItem) => {
+              return (
+                <SimpleGrid
+                  className={styles.in}
+                  columns={5}
+                  spacing={1}
+                  key={noteItem._id}
+                >
+                  <Box
+                    key={noteItem._id}
+                    className={styles.in}
+                    bg="gray.50"
+                    height="80px"
+                  >
+                    {noteItem.Type}
+                  </Box>
+                  <Box
+                    key={noteItem._id.slice(0, 6)}
+                    className={styles.in}
+                    bg="gray.50"
+                    height="80px"
+                  >
+                    {noteItem.Desc}
+                  </Box>
+                  <Box
+                    key={noteItem._id.slice(0, 5)}
+                    className={styles.in}
+                    bg="gray.50"
+                    height="80px"
+                  >
+                    {noteItem.Amt["$numberDecimal"].toLocaleString()}
+                  </Box>
+                  <Box
+                    key={noteItem._id.slice(0, 7)}
+                    className={styles.in}
+                    bg="gray.50"
+                    height="80px"
+                  >
+                    {noteItem.updatedAt.slice(0, 10)}
+                  </Box>
+                  <Box
+                    key={noteItem._id.slice(0, 8)}
+                    className={styles.in}
+                    bg="gray.50"
+                    height="80px"
+                  >
+                    <button
+                      id={noteItem._id}
+                      colorScheme="teal"
+                      onClick={editFunc}
+                      
+                    >
+                      <AiFillEdit style={{fontSize:"2.5vh"}} />
+                    </button>{" "}
+                    &nbsp;&nbsp;{" "}
+                    <button
+                      colorScheme="teal"
+                      id={noteItem._id}
+                      onClick={deleteFunc}
+                    >
+                      <AiFillDelete style={{fontSize:"2.5vh"}} />
+                    </button>
+                  </Box>
+                </SimpleGrid>
+              );
+            })
+          ) : (
+            <></>
+          )}
+          <SimpleGrid columns={2} spacing={1}>
+            <Box className={styles.head} bg="teal" height="80px">
+              Total Income :
+            </Box>
+            <Box className={styles.head} bg="teal" height="80px">
+              INR {amt1 === null ? 0 : amt1}
+            </Box>
+          </SimpleGrid>
+        </Stack>
+      </div>
+    </div>
+  );
+}
 
 export default Inc;
--- src/pages/LineGraph/LineGraph.js
+++ src/pages/LineGraph/LineGraph.js
@@ -1,5 +1,6 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import Chart from 'react-apexcharts';
+import "./graphs.css";
 //install : npm install react-apexcharts apexcharts//
 function Linechart(props)
 {   const dd=new Date();
@@ -15,7 +16,7 @@ function Linechart(props)
     d3.slice(0,9);
     d4.slice(0,9);
     d5.slice(0,9);
-
+    console.log("LINE GRAPH"+d0);
     const[product, setProduct]= useState( [
         {
             name:"Income",
@@ -34,15 +35,91 @@ function Linechart(props)
         title:{ text:"Income, Expense and Investment in last 6 days", align:"center"},
         xaxis:{
             title:{text:"Dates"},
-            categories:[dd.setDate(dd.getDate() - 5),dd.setDate(dd.getDate() - 4),dd.setDate(dd.getDate() - 3),dd.setDate(dd.getDate() - 2),dd.setDate(dd.getDate() - 1),dd.setDate(dd.getDate())]
+            categories:[d5,d4,d3,d2,d1,d0],
+            // categories:[dd.setDate(dd.getDate() - 5),dd.setDate(dd.getDate() - 4),dd.setDate(dd.getDate() - 3),dd.setDate(dd.getDate() - 2),dd.setDate(dd.getDate() - 1),dd.setDate(dd.getDate())]
         },
         yaxis:{
             title:{text:"IN INR"}                 
         }
 
     });
+//     function changeData(){
+//         console.log("DATE"+d5.toLocaleString());
+//         if (props.inc.length<6 || props.exp.length<6 || props.inv1.length<6)
+//         {
+//             setProduct(
+//                 [
+//                     {
+//                         name:"Income",
+//                         data:[0,0,0,0,0,0]
+//                     },
+//                     {
+//                     name:"Expense",
+//                     data:[0,0,0,0,0,0]
+//                     },
+//                     {
+//                     name:"Investment",
+//                     data:[0,0,0,0,0,0]
+//                     }
+//                 ]
+//             );
 
-    React.useEffect(() => {
+//             setOption(
+//                 {
+//                     title:{ text:"Income, Expense and Investment in last 6 days", align:"center"},
+//                     xaxis:{
+//                         title:{text:"Dates"},
+//                         categories:[d5,d4,d3,d2,d1,d0],
+//                         // categories:[dd.setDate(dd.getDate() - 5).toLocaleString(),dd.setDate(dd.getDate() - 4).toLocaleString(),dd.setDate(dd.getDate() - 3).toLocaleString(),dd.setDate(dd.getDate() - 2).toLocaleString(),dd.setDate(dd.getDate() - 1).toLocaleString(),dd.setDate(dd.getDate()).toLocaleString()]
+//                     },
+//                     yaxis:{
+//                         title:{text:"IN INR"}                 
+//                     }
+
+//                 });
+//             }
+    
+        
+//         else{
+//             setProduct(
+//                 [
+//                     {
+//                         name:"Income",
+//                         data:[props.inc[5],props.inc[4],props.inc[3],props.inc[2],props.inc[1],props.inc[0]]
+//                     },
+//                     {
+//                     name:"Expense",
+//                     data:[props.exp[5],props.exp[4],props.exp[3],props.exp[2],props.exp[1],props.exp[0]]
+//                     },
+//                     {
+//                     name:"Investment",
+//                     data:[props.inv1[5],props.inv1[4],props.inv1[3],props.inv1[2],props.inv1[1],props.inv1[0]]
+//                     }
+//                 ]
+//             );
+
+//             setOption(
+//                 {
+//                     title:{ text:"Income, Expense and Investment in last 6 days", align:"center"},
+//                     xaxis:{
+//                         title:{text:"Dates"},
+//                         categories:[d5,d4,d3,d2,d1,d0]
+//                     },
+//                     yaxis:{
+//                         title:{text:"IN INR"}                 
+//                     }
+
+//                 });
+//                 i++;
+//                 console.log("i is "+i);
+//     }
+// }
+// var intervalId = window.setInterval(function(){
+//     // call your function here
+//     changeData();
+//   }, 5000);
+
+    useEffect(() => {
         console.log("DATE"+d5.toLocaleString());
         if (props.inc.length<6 || props.exp.length<6 || props.inv1.length<6)
         {
@@ -68,7 +145,8 @@ function Linechart(props)
                     title:{ text:"Income, Expense and Investment in last 6 days", align:"center"},
                     xaxis:{
                         title:{text:"Dates"},
-                        categories:[dd.setDate(dd.getDate() - 5).toLocaleString(),dd.setDate(dd.getDate() - 4).toLocaleString(),dd.setDate(dd.getDate() - 3).toLocaleString(),dd.setDate(dd.getDate() - 2).toLocaleString(),dd.setDate(dd.getDate() - 1).toLocaleString(),dd.setDate(dd.getDate()).toLocaleString()]
+                        categories:[d5,d4,d3,d2,d1,d0],
+                        // categories:[dd.setDate(dd.getDate() - 5).toLocaleString(),dd.setDate(dd.getDate() - 4).toLocaleString(),dd.setDate(dd.getDate() - 3).toLocaleString(),dd.setDate(dd.getDate() - 2).toLocaleString(),dd.setDate(dd.getDate() - 1).toLocaleString(),dd.setDate(dd.getDate()).toLocaleString()]
                     },
                     yaxis:{
                         title:{text:"IN INR"}                 
@@ -108,16 +186,23 @@ function Linechart(props)
                     }
 
                 });
-        }},[props.inc,props.exp,props.inv1]);
-
+                i++;
+                console.log("i is "+i)
+        }},[props.inc,props.exp,props.inv1,i]);
+        var i=0;
+        let screenWidth = window.screen.width;
+  useEffect(()=>{
+    console.log(screenWidth)
+  },[])
     return(<React.Fragment>
         <div className='container-fluid mt-3 mb-3'>
                      
           <Chart type='line'
-          width={700}
-          height={550}
+          width={(screenWidth>800) ? "700" : "320"}
+          height={(screenWidth>800) ? "550" : "600"}
           series={product}
           options={option }
+          className="chartss"
           >
           </Chart>
 
